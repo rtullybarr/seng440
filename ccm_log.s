4ffce94742df3070120aa275549ad387c75da251
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2018-07-19 11:17:06 -0700

    Adding assembly file (ccm_log.s) compiled using arm-linux-gcc -std=c99 -S -O3 -o ccm_log.s ccm_log.c -lm

--- ccm_log.s
+++ ccm_log.s
@@ -6,140 +6,91 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 2
 	.eabi_attribute 18, 4
 	.file	"ccm_log.c"
-	.global	__aeabi_i2d
-	.global	__aeabi_dadd
-	.global	__aeabi_d2f
 	.text
-	.align	2
-	.global	populate_lookup_table
-	.type	populate_lookup_table, %function
-populate_lookup_table:
-	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 8
-	@ frame_needed = 1, uses_anonymous_args = 0
-	stmfd	sp!, {r4, r5, r6, r7, fp, lr}
-	add	fp, sp, #20
-	sub	sp, sp, #8
-	mov	r3, #0
-	str	r3, [fp, #-24]
-	b	.L2
-.L3:
-	ldr	r7, [fp, #-24]
-	ldr	r3, [fp, #-24]
-	rsb	r3, r3, #0
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, #0
-	mov	r1, #1073741824
-	mov	r2, r3
-	mov	r3, r4
-	bl	pow
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, #0
-	mov	r6, #1069547520
-	add	r6, r6, #3145728
-	mov	r0, r3
-	mov	r1, r4
-	mov	r2, r5
-	mov	r3, r6
-	bl	__aeabi_dadd
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	log2
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	__aeabi_d2f
-	mov	r2, r0
-	ldr	r3, .L5
-	str	r2, [r3, r7, asl #2]	@ float
-	ldr	r3, [fp, #-24]
-	add	r3, r3, #1
-	str	r3, [fp, #-24]
-.L2:
-	ldr	r3, [fp, #-24]
-	cmp	r3, #31
-	ble	.L3
-	sub	sp, fp, #20
-	ldmfd	sp!, {r4, r5, r6, r7, fp, lr}
-	bx	lr
-.L6:
-	.align	2
-.L5:
-	.word	lookup_table
-	.size	populate_lookup_table, .-populate_lookup_table
-	.global	__aeabi_fsub
 	.align	2
 	.global	ccm_log
 	.type	ccm_log, %function
 ccm_log:
 	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 24
-	@ frame_needed = 1, uses_anonymous_args = 0
-	stmfd	sp!, {fp, lr}
-	add	fp, sp, #4
-	sub	sp, sp, #24
-	str	r0, [fp, #-24]
-	ldr	r3, .L12	@ float
-	str	r3, [fp, #-20]	@ float
-	mov	r3, #0
-	str	r3, [fp, #-16]
-	ldr	r3, .L12	@ float
-	str	r3, [fp, #-12]	@ float
-	mov	r3, #0
-	str	r3, [fp, #-8]
-	b	.L8
-.L10:
-	ldr	r2, [fp, #-24]
-	ldr	r3, [fp, #-8]
-	mov	r2, r2, lsr r3
-	ldr	r3, [fp, #-24]
-	add	r3, r2, r3
-	str	r3, [fp, #-16]
-	ldr	r2, [fp, #-8]
-	ldr	r3, .L12+4
-	ldr	r2, [r3, r2, asl #2]	@ float
-	ldr	r3, [fp, #-20]	@ float
-	mov	r0, r3
-	mov	r1, r2
-	bl	__aeabi_fsub
-	mov	r3, r0
-	str	r3, [fp, #-12]	@ float
-	ldr	r3, [fp, #-16]
-	cmp	r3, #1073741824
-	bhi	.L9
-	ldr	r3, [fp, #-16]
-	str	r3, [fp, #-24]
-	ldr	r3, [fp, #-12]	@ float
-	str	r3, [fp, #-20]	@ float
-.L9:
-	ldr	r3, [fp, #-8]
-	add	r3, r3, #1
-	str	r3, [fp, #-8]
-.L8:
-	ldr	r3, [fp, #-8]
-	cmp	r3, #31
-	ble	.L10
-	ldr	r3, [fp, #-20]	@ float
-	mov	r0, r3
-	sub	sp, fp, #4
-	ldmfd	sp!, {fp, lr}
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	r4, [sp, #-4]!
+	ldr	r4, .L8
+	ldr	r3, [r4, #0]
+	mov	ip, #0
+	rsb	r3, r3, #0
+	mov	r1, r0, asl #1
+	mov	r2, ip
+	b	.L4
+.L7:
+	add	r1, r0, r0, lsr r2
+.L4:
+	cmp	r1, #1073741824
+	add	r2, r2, #1
+	movls	ip, r3
+	ldr	r3, [r4, r2, asl #2]
+	movls	r0, r1
+	cmp	r2, #31
+	rsb	r3, r3, ip
+	bne	.L7
+	mov	r0, ip
+	ldmfd	sp!, {r4}
 	bx	lr
-.L13:
+.L9:
 	.align	2
-.L12:
-	.word	0
+.L8:
 	.word	lookup_table
 	.size	ccm_log, .-ccm_log
-	.comm	lookup_table,128,4
+	.global	__aeabi_i2d
+	.global	__aeabi_dadd
+	.global	__aeabi_d2f
+	.global	__aeabi_fmul
+	.global	__aeabi_f2iz
+	.align	2
+	.global	populate_lookup_table
+	.type	populate_lookup_table, %function
+populate_lookup_table:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	stmfd	sp!, {r4, r5, r6, lr}
+	mov	r4, #0
+	ldr	r6, .L14
+	mov	r5, r4
+.L11:
+	mov	r0, r4
+	bl	__aeabi_i2d
+	mov	r2, r0
+	mov	r3, r1
+	mov	r0, #0
+	mov	r1, #1073741824
+	bl	pow
+	mov	r3, #1069547520
+	mov	r2, #0
+	add	r3, r3, #3145728
+	bl	__aeabi_dadd
+	bl	log2
+	bl	__aeabi_d2f
+	mov	r1, #1308622848
+	add	r1, r1, #8388608
+	bl	__aeabi_fmul
+	bl	__aeabi_f2iz
+	sub	r4, r4, #1
+	cmn	r4, #30
+	str	r0, [r6, r5]
+	add	r5, r5, #4
+	bne	.L11
+	ldmfd	sp!, {r4, r5, r6, lr}
+	bx	lr
+.L15:
+	.align	2
+.L14:
+	.word	lookup_table
+	.size	populate_lookup_table, .-populate_lookup_table
+	.comm	lookup_table,124,4
 	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
 	.section	.note.GNU-stack,"",%progbits

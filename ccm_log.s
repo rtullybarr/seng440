180426ada7637525ff1c38e0d65eb1e2b4e2286c
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2018-07-22 18:52:54 -0700

    Adding assembly corresponding to inlined function version of ccm_log.

--- ccm_log.s
+++ ccm_log.s
@@ -8,60 +8,28 @@
 	.eabi_attribute 26, 2
 	.eabi_attribute 30, 2
 	.eabi_attribute 18, 4
-	.file	"ccm_log.c"
-	.text
-	.align	2
-	.global	ccm_log
-	.type	ccm_log, %function
-ccm_log:
-	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 0
-	@ frame_needed = 0, uses_anonymous_args = 0
-	@ link register save eliminated.
-	str	r4, [sp, #-4]!
-	ldr	r4, .L8
-	ldr	r3, [r4, #0]
-	mov	ip, #0
-	rsb	r3, r3, #0
-	mov	r1, r0, asl #1
-	mov	r2, ip
-	b	.L4
-.L7:
-	add	r1, r0, r0, lsr r2
-.L4:
-	cmp	r1, #1073741824
-	add	r2, r2, #1
-	movls	ip, r3
-	ldr	r3, [r4, r2, asl #2]
-	movls	r0, r1
-	cmp	r2, #31
-	rsb	r3, r3, ip
-	bne	.L7
-	mov	r0, ip
-	ldmfd	sp!, {r4}
-	bx	lr
-.L9:
-	.align	2
-.L8:
-	.word	lookup_table
-	.size	ccm_log, .-ccm_log
+	.file	"test_ccm_log.c"
 	.global	__aeabi_i2d
 	.global	__aeabi_dadd
 	.global	__aeabi_d2f
 	.global	__aeabi_fmul
 	.global	__aeabi_f2iz
+	.global	__aeabi_i2f
+	.global	__aeabi_f2d
+	.text
 	.align	2
-	.global	populate_lookup_table
-	.type	populate_lookup_table, %function
-populate_lookup_table:
+	.global	main
+	.type	main, %function
+main:
 	@ Function supports interworking.
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
 	stmfd	sp!, {r4, r5, r6, lr}
 	mov	r4, #0
-	ldr	r6, .L14
+	ldr	r6, .L10+8
+	sub	sp, sp, #8
 	mov	r5, r4
-.L11:
+.L2:
 	mov	r0, r4
 	bl	__aeabi_i2d
 	mov	r2, r0
@@ -83,14 +51,57 @@ populate_lookup_table:
 	cmn	r4, #30
 	str	r0, [r6, r5]
 	add	r5, r5, #4
-	bne	.L11
+	bne	.L2
+	ldr	r3, .L10+8
+	mov	ip, #641728512
+	ldr	r2, [r3, #0]
+	mov	r3, #1275068416
+	add	r3, r3, #13369344
+	add	ip, ip, #2506752
+	mov	r1, #0
+	rsb	r2, r2, #0
+	add	r3, r3, #52480
+	add	ip, ip, #9856
+	mov	r0, r1
+	b	.L5
+.L9:
+	add	r3, ip, ip, lsr r1
+.L5:
+	cmp	r3, #1073741824
+	add	r1, r1, #1
+	movls	ip, r3
+	ldr	r3, [r6, r1, asl #2]
+	movls	r0, r2
+	cmp	r1, #31
+	rsb	r2, r3, r0
+	bne	.L9
+	bl	__aeabi_i2f
+	mov	r1, #813694976
+	bl	__aeabi_fmul
+	bl	__aeabi_f2d
+	adr	r5, .L10
+	ldmia	r5, {r4-r5}
+	mov	r2, r0
+	mov	r3, r1
+	ldr	r0, .L10+12
+	stmia	sp, {r4-r5}
+	bl	printf
+	mov	r0, #0
+	add	sp, sp, #8
 	ldmfd	sp!, {r4, r5, r6, lr}
 	bx	lr
-.L15:
-	.align	2
-.L14:
+.L11:
+	.align	3
+.L10:
+	.word	-1075836637
+	.word	-1075342024
 	.word	lookup_table
-	.size	populate_lookup_table, .-populate_lookup_table
+	.word	.LC0
+	.size	main, .-main
 	.comm	lookup_table,124,4
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"ccm=%f, log2=%f\012\000"
 	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
 	.section	.note.GNU-stack,"",%progbits
--- test_ccm_log.c
+++ test_ccm_log.c
@@ -7,10 +7,9 @@ int main (void) {
     // M is a normalized fixed-point number
 
     populate_lookup_table();
-    float m;
+    float m = 0.6;
 
-    for (int i = 0; i < 100; i++) {
-        for (m = 0.5; m <= 1.0; m += 0.0000001) {
+    //for (m = 0.5; m <= 1.0; m += 0.0000001) {
             // convert to fixed-point
             unsigned int M = (unsigned int)(m * SCALE_FACTOR);
 
@@ -19,7 +18,7 @@ int main (void) {
             // convert from fixed-point
             float log_m = (log_M) / (float)SCALE_FACTOR;
 
-            //printf("ccm=%f, log2=%f\n", log_m, log2(m));
-        }
-    }
+            printf("ccm=%f, log2=%f\n", log_m, log2(m));
+        //}
+    //}
 }

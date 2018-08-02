5125b20532f8149b66117c7b8b55940679dd966f
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2018-08-01 19:31:08 -0700

    Asm final version.

--- ccm_log.s
+++ ccm_log.s
@@ -8,65 +8,33 @@
 	.eabi_attribute 26, 2
 	.eabi_attribute 30, 2
 	.eabi_attribute 18, 4
-	.file	"ccm_log.c"
-	.text
-	.align	2
-	.global	ccm_log
-	.type	ccm_log, %function
-ccm_log:
-	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 0
-	@ frame_needed = 0, uses_anonymous_args = 0
-	@ link register save eliminated.
-	str	r4, [sp, #-4]!  @ preserve r4
-	ldr	r4, .L8			@ location of LUT
-	ldr	r3, [r4, #0]    @ first element of LUT to r3 (r3 = theta)
-	mov	ip, #0			@ move 0 to scratch register (f = 0)
-	rsb	r3, r3, #0		@ r3 = -r3 					(theta = f(==0) - LUT[0])
-	mov	r1, r0, asl #1  @ r1 = r0 << 1 				(u = M + M -> line24)
-	mov	r2, ip			@ r2 = 0 					(i = 0)
-	b	.L4				@ branch to L4              (go to for loop)
-.L7:
-	add	r1, r0, r0, lsr r2 @ r1 = r0 + (r0 >> r2) 	(u = M + (M >> i))
-.L4:
-	cmp	r1, #1073741824 @ u <= SCALE_FACTOR?
-	add	r2, r2, #1		@ increase counter  		(i = i+1)
-	movls	ip, r3		@ ip = r3 if r1 <= SF       (f = theta if u <=SCALE_FACTOR)
-	ldr	r3, [r4, r2, asl #2] @ r3 = next LUT elem   (theta = LUT[i])
-	movls	r0, r1      @ r0 = r1 if r1 <= SF       (M = u if u <= SCALE_FACTOR)
-	cmp	r2, #31			@ i == 32?
-	rsb	r3, r3, ip		@ r3 = ip - r3				(theta = f - LUT[i])
-	bne	.L7				@ if i != 31, update r1     (update u, repeat loop)   
-	mov	r0, ip			@ r0 = ip					(return f)
-	ldmfd	sp!, {r4}   @ load r4, return
-	bx	lr				@ PC = link register
-.L9:
-	.align	2
-.L8:					@ find location of LUT, populate
-	.word	lookup_table
-	.size	ccm_log, .-ccm_log
+	.file	"perf_test_ccm_log.c"
 	.global	__aeabi_i2d
 	.global	__aeabi_dadd
 	.global	__aeabi_d2f
 	.global	__aeabi_fmul
 	.global	__aeabi_f2iz
+	.global	__aeabi_i2f
+	.global	__aeabi_f2d
+	.text
 	.align	2
-	.global	populate_lookup_table
-	.type	populate_lookup_table, %function
-populate_lookup_table:
+	.global	main
+	.type	main, %function
+main:
 	@ Function supports interworking.
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
-	stmfd	sp!, {r4, r5, r6, lr} 	@ stack all registers and return addr
-	mov	r4, #0						@ r4 = 0 (&LUT = 0)
-	ldr	r6, .L14					@ r6 = LUT properties??
-	mov	r5, r4						@ r5 = 0
-.L11:
-	mov	r0, r4						@ r0 = 0
-	bl	__aeabi_i2d					@ convert r0 uint32 -> 64 bit float
-	mov	r2, r0						@ r2 = float(0)
-	mov	r3, r1						@ r3 = r1 (r1 = ??)
-	mov	r0, #0						@ 
+	stmfd	sp!, {r4, r5, r6, lr}
+	mov	r4, #0
+	ldr	r66 .L10+8
+	sub	sp, sp, #8
+	mov	r5, r4
+.L2:
+	mov	r0, r4
+	bl	__aeabi_i2d
+	mov	r2, r0
+	mov	r3, r1
+	mov	r0, #0
 	mov	r1, #1073741824
 	bl	pow
 	mov	r3, #1069547520
@@ -83,14 +51,55 @@ populate_lookup_table:
 	cmn	r4, #30
 	str	r0, [r6, r5]
 	add	r5, r5, #4
-	bne	.L11
+	bne	.L2
+	mov	r3, #1275068416
+	mov	r1, #641728512
+	mov	r2, #0
+	add	r3, r3, #13369344
+	add	r1, r1, #2506752
+	add	r3, r3, #52480
+	add	r1, r1, #9856
+	mov	r0, r2
+	mov	ip, r2
+	b	.L5
+.L9:
+	add	r3, r1, r1, lsr r2
+.L5:
+	cmp	r3, #1073741824
+	add	r2, r2, #1
+	movls	r1, r3
+	ldr	r3, [r6, r2, asl #2]
+	movls	r0, ip
+	cmp	r2, #31
+	rsb	ip, r3, r0
+	bne	.L9
+	bl	__aeabi_i2f
+	mov	r1, #813694976
+	bl	__aeabi_fmul
+	bl	__aeabi_f2d
+	adr	r5, .L10
+	ldmia	r5, {r4-r5}
+	mov	r2, r0
+	mov	r3, r1
+	ldr	r0, .L10+12
+	stmia	sp, {r4-r5}
+	bl	printf
+	mov	r0, #0
+	add	sp, sp, #8
 	ldmfd	sp!, {r4, r5, r6, lr}
 	bx	lr
-.L15:
-	.align	2
-.L14:
+.L11:
+	.align	3
+.L10:
+	.word	-1075836637
+	.word	-1075342024
 	.word	lookup_table
-	.size	populate_lookup_table, .-populate_lookup_table
+	.word	.LC0
+	.size	main, .-main
 	.comm	lookup_table,124,4
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"ccm= %.10f, log2= %.10f\012\000"
 	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
 	.section	.note.GNU-stack,"",%progbits

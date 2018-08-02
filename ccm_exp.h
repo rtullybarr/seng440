c271eb0b2e56d2abeed7b29cb216fff66d092dbd
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2018-08-01 19:38:04 -0700

    Final ASM and test file for exp.

--- ccm_exp.h
+++ ccm_exp.h
@@ -4,8 +4,41 @@
 #define PRECISION 30
 #define SCALE_FACTOR (1 << 30)
 
-void populate_lookup_table();
-int ccm_exp(unsigned int M);
+// Note: extra spot in lookup table to accommodate software pipelining.
+int lookup_table[PRECISION + 1] = {
+    1073741824,
+    628098688,
+    345667648,
+    182455584,
+    93912512,
+    47667824,
+    24017256,
+    12055174,
+    6039313,
+    3022600,
+    1512037,
+    756203,
+    378147,
+    189085,
+    94545,
+    47273,
+    23636,
+    11818,
+    5909,
+    2954,
+    1477,
+    738,
+    369,
+    184,
+    92,
+    46,
+    23,
+    11,
+    5,
+    2,
+    0 // Unused
+};
+
 
 // CCM - convergence computing method
 // Calculates the base-2 exponential of M,
--- test_ccm_exp.c
+++ test_ccm_exp.c
@@ -6,8 +6,6 @@ int main (void) {
     // Calculate 2^M, where 0.0 <= M < 1.0
     // M is a positive and pure fractional number
 
-    populate_lookup_table();
-
     for (float m = 0.0; m <= 1.0; m += 0.0000001) {
         // convert to fixed-point
         unsigned int M = (unsigned int)(m * SCALE_FACTOR);
@@ -17,7 +15,7 @@ int main (void) {
         // convert from fixed-point
         float exp_m = (exp_M) / (float)SCALE_FACTOR;
 
-        //printf("ccm= %.10f, exp2= %.10f\n", exp_m, pow(2.0, m));
+        printf("ccm= %.10f, exp2= %.10f\n", exp_m, pow(2.0, m));
     }
 }
 
